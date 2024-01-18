1a33954ec78fce669a6a3d3ab6d1f4f7647aaf68
nick.lin63 <nick.lin@titansoft.com.sg>
2024-01-18 16:50:55 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig((mode) => {
-  // const env = loadEnv(mode, process.cwd(), "")  
-  return {
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
         plugins: [vue()],
         resolve: {
             alias: {
@@ -14,7 +14,7 @@ export default defineConfig((mode) => {
             },
         },
         define: {
-            __API_URL__: "http://localhost:7189" // env.VITE_API_URL,
+            __API_URL__: JSON.stringify(env.VITE_API_URL),
         },
     };
 });
